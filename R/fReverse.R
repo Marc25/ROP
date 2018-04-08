0d578ed44596be56ce700424998b3c46c3aba16a
Marc25 <cestudios.alonso@gmail.com>
2018-04-08 14:41:52 +0200

    Prueba de commit

---
+++ R/fReverse.R
@@ -0,0 +1,14 @@
+
+#' Title Function que inverte un vector pasado como argumento
+#'
+#' @param x
+#'
+#' @return Vector inverse
+#' @return Vector inverse End
+#' @export
+#'
+#' @examples
+#' reverse(1:12)
+reverse <- function(x){
+  if (length(x)>0)  x[length(x):1] else x
+}
